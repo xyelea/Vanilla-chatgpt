bf2f6926b45340ab0088e7c22a24e809731404ff
Sumpah Gw Bukan Lolicon <51400321+xyelea@users.noreply.github.com>
2023-01-08 17:34:51 +0700

    Update script.js
    sa

--- client/script.js
+++ client/script.js
@@ -100,7 +100,6 @@ const handleSubmit = async (e) => {
   if (response.ok) {
     const data = await response.json();
     const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
-
     typeText(messageDiv, parsedData);
   } else {
     const err = await response.text();
